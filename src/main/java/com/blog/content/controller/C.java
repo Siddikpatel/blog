4d34c6ee5bb17fb7fd52e8350f71ee6549ad0cd2
Siddikpatel <patel.siddik369@gmail.com>
2024-10-23 18:16:09 -0300

    second commit

---
+++ src/main/java/com/blog/content/controller/C.java
@@ -0,0 +1,6 @@
+package com.blog.content.controller;
+
+public interface C {
+
+    void printIt();
+}
---
+++ src/main/java/com/blog/content/controller/D.java
@@ -0,0 +1,9 @@
+package com.blog.content.controller;
+
+public class D implements C {
+
+    @Override
+    public void printIt() {
+        System.out.println("Hello");
+    }
+}
